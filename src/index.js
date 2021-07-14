2d201781c470a49be3fa5e6cd0029d8a467c5620
carloso0114 <carlossospinag@gmail.com>
2021-07-14 11:55:09 -0500

    update UI

--- src/index.js
+++ src/index.js
@@ -10,20 +10,24 @@ const showTasks = () => {
     const li = document.createElement('li');
     if (e.completed) {
       li.innerHTML = `
+          <div> 
           <input class="checkbox m-2" type="checkbox" id="${e.index}" checked>
           <label class="description" for="${e.index}">${e.description}</label>
-          <i class="fas fa-ellipsis-v justify-content-end "></i>
+          </div>
+          <i class="fas fa-ellipsis-v"></i>
 
 
         `;
     } else {
       li.innerHTML = `
+          <div> 
           <input class="checkbox m-2" type="checkbox" id="${e.index}">
           <label class="description" for="${e.index}">${e.description}</label>
+          </div>
           <i class="fas fa-ellipsis-v"></i>
         `;
     }
-    li.classList.add('list-group-item');
+    li.classList.add('list-group-item', 'd-flex', 'justify-content-between', 'align-items-center');
     ulSelector.appendChild(li);
   });
 };
