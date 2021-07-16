05e552474af763065a2c3dd56e0d16773b21e760
carloso0114 <carlossospinag@gmail.com>
2021-07-16 00:33:36 -0500

    add bootstrap icons

--- src/modules/showTasks.js
+++ src/modules/showTasks.js
@@ -14,7 +14,7 @@ const showTasks = () => {
       <input class="checkbox m-2" type="checkbox" id="${e.index}" checked>
       <label for="${e.index}">${e.description}</label>
       </div>
-      <i class="fas fa-ellipsis-v" ></i>
+      <i class="fs-5 bi bi-three-dots-vertical"></i>
       `;
     } else {
       li.innerHTML = `
@@ -22,7 +22,7 @@ const showTasks = () => {
       <input class="checkbox m-2" type="checkbox" id="${e.index}">
       <label for="${e.index}">${e.description}</label>
       </div>
-      <i class="fas fa-ellipsis-v" ></i>
+      <i class="fs-5 bi bi-three-dots-vertical"></i>
       `;
     }
     li.classList.add('list-group-item', 'draggable', 'droppable', 'd-flex', 'justify-content-between', 'align-items-center');
